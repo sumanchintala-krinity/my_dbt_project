9475b2fac2778fafcb6476bffb1d39ac337db7b5
suman chintala <suman.chintala@krinitysolutions.com>
2026-05-09 20:03:25 +0000

    new files

--- models/example/orders.sql
+++
@@ -1,9 +0,0 @@
-select
-    order_id,  -- This was 'id' in your previous version
-    user_id,
-    status,
-    created_at,
-    shipped_at,
-    delivered_at,
-    num_of_item
-from {{ source('thelook_ecommerce', 'orders') }}
